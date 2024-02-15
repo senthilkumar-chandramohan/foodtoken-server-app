70010e79636343b40d035d983a104017c8b3a091
Senthilkumar Chandramohan <senthilkumar.chandramohan@outlook.com>
2024-02-16 02:32:20 +0530

    Looking-up firebase service account key from env now.

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,7 +1,9 @@
 import firebaseAdmin from "firebase-admin";
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require("./serviceAccountKey.json");
+// const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccountKey = (process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '').replace(/nl/g, "");
+const serviceAccount = JSON.parse(serviceAccountKey);
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
 });
